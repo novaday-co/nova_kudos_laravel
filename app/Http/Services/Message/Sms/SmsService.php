1be12ccb0c6f8a56f2e366d45441c525724d25e2
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-24 14:57:11 +0330

    fix sms service

--- app/Http/Services/Message/Sms/SmsService.php
+++ app/Http/Services/Message/Sms/SmsService.php
@@ -7,14 +7,13 @@ use Kavenegar\KavenegarApi;
 
 class SmsService implements MessageInterface
 {
-    private string $sender = '1000596446';
     private string $receptor;
     private string $message;
 
     public function fire()
     {
-        $kavenegarSystem = new KavenegarApi('516F5552376F74586A30642F49734739546770324B554632513146726B466A6D443732624D6179304A474D3D');
-        $kavenegarSystem->Send($this->sender, $this->receptor, $this->message);
+        $kavenegarSystem = new KavenegarApi(env('KAVENEGAR_API_KEY'));
+        $kavenegarSystem->Send(env('KAVENEGAR_SENDER_SMS'), $this->receptor, $this->message);
     }
 
     public function getReceptor(): string
