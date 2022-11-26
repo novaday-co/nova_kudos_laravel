4f0aa26977aa6ada038cf54f914e8b43b1514c03
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-26 14:18:48 +0330

    define role and permission

---
+++ app/Http/Controllers/Api/v1/Admin/User/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Admin\User;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
