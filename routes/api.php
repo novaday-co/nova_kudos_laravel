003ba2cf7a3faa5a84721937802e608f4e467c58
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-27 14:43:16 +0330

    define user routes and add sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\Admin\User\UserController;
 use App\Http\Controllers\Api\v1\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,18 @@ Route::prefix('v1')->name('v1.authentication.')->group(function (){
     // login & register route
     Route::post('login-register', [AuthController::class, 'login'])->name('login');
     Route::post('check-otp', [AuthController::class, 'checkOtp'])->name('check.otp');
+
+    // admin routes
+    Route::prefix('admin')->middleware('auth:sanctum')->name('v1.authentication.admin.')->group(function (){
+        // user routes
+        Route::prefix('users')->name('user.')->group(function (){
+            Route::get('all', [UserController::class, 'index'])->name('all');
+            Route::post('store', [UserController::class, 'store'])->name('store');
+            Route::put('update/{user}', [UserController::class, 'update'])->name('update');
+            Route::delete('delete/{user}', [UserController::class, 'destroy'])->name('destroy');
+        });
+        // final admin routes
+    });
 });
 
 Route::post('add/user/{user}/to-group/{group}', [\App\Http\Controllers\Api\v1\Admin\Group\GroupController::class, 'addUser']);
