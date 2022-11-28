00ced10ecd1e80d05307f97ce22318c98a7cbd13
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-28 21:57:31 +0330

    fix question resource

--- app/Http/Resources/Admin/QuestionResource.php
+++ app/Http/Resources/Admin/QuestionResource.php
@@ -14,6 +14,8 @@ class QuestionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'title' => $this->title
+        ];
     }
 }
