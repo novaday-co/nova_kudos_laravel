468bdabcfec78acc6147f8eb27247722ca771b17
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-12-02 18:59:45 +0330

    add product and gift card

---
+++ app/Http/Controllers/Api/Admin/GiftCard/GiftCardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin\GiftCard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GiftCardController extends Controller
+{
+    //
+}
