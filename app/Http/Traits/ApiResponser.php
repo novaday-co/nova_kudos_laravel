29c3fae5fb514ad658c5e090f7dfce301f4c453f
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-12-13 10:27:46 +0330

    Create: ApiResponser #2

--- app/Http/Traits/ApiResponser.php
+++ app/Http/Traits/ApiResponser.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Traits;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 trait ApiResponser
 {
+    public function success($data, string $message, int $code = 200): JsonResponse
+    {
+        return response()->json(['data' => $data, 'message' => $message, 'code' => $code], $code, [], JSON_UNESCAPED_UNICODE);
+    }
 
+    public function error($data, string $message, int $code): JsonResponse
+    {
+        return response()->json(['error' => $data, 'message' => $message, 'code' => $code], $code, [], JSON_UNESCAPED_UNICODE);
+    }
 }
