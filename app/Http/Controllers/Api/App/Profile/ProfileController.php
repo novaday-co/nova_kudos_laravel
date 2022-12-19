7973af7e74f2f4667f6c14898b7f6dd28573a856
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-12-19 22:29:40 +0330

    feature: update profile #5

--- app/Http/Controllers/Api/App/Profile/ProfileController.php
+++ app/Http/Controllers/Api/App/Profile/ProfileController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api\App\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Company\Profile\UpdateAvatarRequest;
+use App\Http\Requests\Admin\Company\Profile\UpdateMobileRequest;
 use App\Http\Requests\Admin\Profile\ProfileRequest;
+use App\Http\Requests\Auth\OtpRequest;
 use App\Http\Resources\UserResource;
 use App\Http\Services\Image\ImageService;
+use App\Http\Services\Message\MessageService;
+use App\Http\Services\Message\Sms\SmsService;
 use App\Models\Company;
+use App\Models\TempMobile;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,9 +23,9 @@ class ProfileController extends Controller
 
     /**
      * @OA\Post(
-     *      path="/api/app/profiles/users/companies/{company_id}",
+     *      path="/api/profiles/companies/{company_id}/users/avatar",
      *      operationId="update profile user",
-     *      tags={"profiles"},
+     *      tags={"Profiles"},
      *      summary="update profile user",
      *      description="update profile user",
      *      security={ {"sanctum": {} }},
@@ -64,7 +71,6 @@ class ProfileController extends Controller
      *              @OA\Schema(
      *                  required={"mobile"},
      *                  @OA\Property(property="avatar", type="file", format="text"),
-     *                  @OA\Property(property="mobile", type="text", format="text", example="09124068701"),
      *               ),
      *           ),
      *       ),
@@ -87,7 +93,7 @@ class ProfileController extends Controller
      *      ),
      * )
      */
-    public function updateProfile(ProfileRequest $request, ImageService $imageService, Company $company_id)
+    public function updateProfile(UpdateAvatarRequest $request, ImageService $imageService, Company $company_id)
     {
         try
         {
@@ -107,7 +113,183 @@ class ProfileController extends Controller
             return response('ok');
         } catch (\Exception $e)
         {
-            return response(['bad request' => $e->getMessage()], 400);
+            return response(['bad request' =>$e->getMessage()], 400);
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/profiles/users/update/mobile",
+     *      operationId="update mobile",
+     *      tags={"Profiles"},
+     *      summary="update mobile",
+     *      description="update mobile",
+     *      security={ {"sanctum": {} }},
+     *      @OA\Parameter(
+     *          name="locale",
+     *          in="header",
+     *          required=true,
+     *          example="fa",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="Accept",
+     *          in="header",
+     *          required=true,
+     *          example="application/json",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="Content-Type",
+     *          in="header",
+     *          required=true,
+     *          example="application/json",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  required={"mobile"},
+     *                  @OA\Property(property="mobile", type="text", format="text", example="09350000000"),
+     *               ),
+     *           ),
+     *       ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(ref="/api/profiles/companies/{company_id}/update/mobile")
+     *       ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="unauthorized",
+     *      ),
+     *     @OA\Response(
+     *          response=422,
+     *          description="error",
+     *       ),
+     *     @OA\Response(
+     *          response=500,
+     *          description="server error",
+     *      ),
+     * )
+     */
+    public function updateMobile(UpdateMobileRequest $request)
+    {
+        try {
+            $attributes = $request->validated();
+            $userMobile = auth()->user()->mobile;
+            if ($userMobile != $attributes['mobile'])
+            {
+                $otpCode = mt_rand(1000, 9999);
+                $tempMobile = TempMobile::query()->updateOrCreate([
+                    'user_id' => auth()->user()->id,
+                    'mobile' => $attributes['mobile'],
+                ],[
+                    'otp_code' => $otpCode,
+                    'expiration_otp' => Carbon::now()->addMinutes(),
+                ]);
+                $smsService = new SmsService();
+                $smsService->setReceptor($tempMobile->mobile);
+                $smsService->setOtpCode($otpCode);
+                $messageService = new MessageService($smsService);
+                $messageService->send();
+            }
+            return $this->success(['otp_code' => $otpCode]);
+        } catch (\Exception $e)
+        {
+            return $this->error(['error: ' => $e->getMessage()], trans('messages.profile.duplicate.mobile'), 422);
+        }
+    }
+    /**
+     * @OA\Post(
+     *      path="/api/profiles/users/verify/mobile",
+     *      operationId="verify mobile",
+     *      tags={"Profiles"},
+     *      summary="verify mobile",
+     *      description="verify mobile",
+     *      security={ {"sanctum": {} }},
+     *      @OA\Parameter(
+     *          name="locale",
+     *          in="header",
+     *          required=true,
+     *          example="fa",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="Accept",
+     *          in="header",
+     *          required=true,
+     *          example="application/json",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="Content-Type",
+     *          in="header",
+     *          required=true,
+     *          example="application/json",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  required={"mobile"},
+     *                  required={"otp_code"},
+     *                  @OA\Property(property="mobile", type="text", format="text", example="09350000000"),
+     *                  @OA\Property(property="otp_code", type="text", format="text", example="1234"),
+     *               ),
+     *           ),
+     *       ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(ref="/api/profiles/companies/{company_id}/update/mobile")
+     *       ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="unauthorized",
+     *      ),
+     *     @OA\Response(
+     *          response=422,
+     *          description="error",
+     *       ),
+     *     @OA\Response(
+     *          response=500,
+     *          description="server error",
+     *      ),
+     * )
+     */
+
+    public function verifyMobile(OtpRequest $request)
+    {
+        try {
+            $attributes = $request->validated();
+            $tempMobile = TempMobile::query()->where('mobile', $attributes['mobile'])
+                ->where('otp_code', $attributes['otp_code'])
+                ->where('expiration_otp', ">=", Carbon::now())->firstOrFail();
+            $user = auth()->user();
+            $user->update([
+                'mobile' => $tempMobile['mobile']
+            ]);
+            return UserResource::make($user);
+        } catch (\Exception $e)
+        {
+            return $this->error([$e->getMessage()], trans('messages.profile.invalid.verify'), 422);
         }
     }
 }
