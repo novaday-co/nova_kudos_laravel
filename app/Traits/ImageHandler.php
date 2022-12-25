813b4b8e7ec87610284d9b86f7b1850124636354
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-12-25 16:53:53 +0330

    bug: fix image handler bug

--- app/Traits/ImageHandler.php
+++ app/Traits/ImageHandler.php
@@ -15,8 +15,15 @@ trait ImageHandler
     {
         if ($file)
         {
-
-
+            $extension = $file->getClientOriginalExtension();
+            $file_name = date('Y')
+                . DIRECTORY_SEPARATOR . date('m') . DIRECTORY_SEPARATOR
+                . date('d') . DIRECTORY_SEPARATOR . time() . '.' . $extension;
+            $pathAddres = $file->storeAs($path, $file_name, 'public');
+            $final_path = '/' . $pathAddres;
+            Image::make($file->getRealPath());
+            return $final_path;
+            // $file_path = '/' . $path . '/' . $file_name;
        }
 }
 }
