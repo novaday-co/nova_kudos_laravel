9053702ead23c824afb55448cb5f37e4499951d0
yasinbaghban <ysnbgn.bgn@gmail.com>
2023-01-11 16:31:40 +0330

    fix: product resource

--- app/Http/Resources/Company/product/AddProductResource.php
+++ app/Http/Resources/Company/product/AddProductResource.php
@@ -15,9 +15,6 @@ class AddProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-      //      'transaction_detail' => UserProductTransactionResource::collection($this->companyUserProductTransactions),
-           // 'user_coin_detail' => CompanyUserDetailResource::collection($this->companies)
-          //  'coin_amount' => $this->pivot->coin_amount,
             'tracking_code' => $this->tracking_code,
             'coin_amount' => $this->user->defaultCompany->coin_amount
         ];
